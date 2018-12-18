a7c4ad4ce9038f1c5f6681ff477f636e086bf21f
Colin Parsons <colinparsons@colins-mbp-6.dhcp.wustl.edu>
2018-12-18 13:44:44 -0600

    added pp and tos; still need to include links to them on the site

---
+++ terms.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400">
+  <link rel="stylesheet" href="styles/styles.css">
+  <link rel="stylesheet" href="styles/internal-styles.css">
+  <link rel="stylesheet" href="styles/legal-styles.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <title>warmglue: terms</title>
+  <meta charset="utf-8">
+  <meta name="description" content="View warmglue's terms of use.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- favicon stuff -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+  <link rel="manifest" href="assets/favicon/site.webmanifest">
+  <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#b30000">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+</head>
+<body>
+  <nav class="navbar">
+    <div class="brand-image">
+      <a href="home">
+        <button class="button brand-image-button">
+          <img class="logo" alt ="">
+        </button>
+      </a>
+    </div>
+  </nav>
+  <div class="main-body">
+    <h1>terms</h1>
+    <br><br><br><?php include('terms.txt'); ?><br><br>
+    <div class="post-link"></div>
+  </div>
+  <div class="footer">
+    <?php
+      echo sprintf("<p>&copy; warmglue %s</p>", date("Y"));
+    ?>
+    <a href="https://twitter.com/realwarmglue" class="medium-link" target="_blank">
+      <img class="twitter" alt="">
+    </a>
+    <a href="https://www.facebook.com/realwarmglue" class="facebook-link" target="_blank">
+      <img class="facebook" alt="">
+    </a>
+    <a href="https://medium.com/@warmglue" class="medium-link" target="_blank">
+      <img class="medium" alt="">
+    </a>
+  </div>
+</body>
+</html>
